b6f0b21973274093e0f2e022d24ea3750d3ef243
se <selcukermaya@gmail.com>
2018-06-27 19:28:19 +0300

    Run it!

---
+++ handler.js
@@ -0,0 +1,76 @@
+const restify = require("restify");
+const fetch = require("node-fetch");
+
+// MonoPush Group Url
+// const pushUrl = "https://webhook.monopush.io/telegram/5b33b5ef09a14000015e20f1";
+// MonoPush Personal Url
+const pushUrl = "https://webhook.monopush.io/telegram/5b155d74be31050001905472";
+
+function respond(req, res, next) {
+  const query = req.query;
+  const headers = req.headers;
+  if (!headers || !headers["x-secret"] || headers["x-secret"] != process.env.SECRET) {
+    res.send(403, "You are not authorized for this request.");
+    res.end();
+    return;
+  }
+
+  if (query.file) {
+    var url = `${query.file}`;
+    fetch(url)
+      .then(res => res.text())
+      .then(body => {
+        const content = { message: body };
+
+        content.message = content.message.replace(/\(YANINDA[\+| ]PİLAV\+YOĞURT\+SU\)\n/gi, "");
+        content.message = content.message.replace(/\d+[,]\d+\sTL[\n]/gi, "");
+        content.message = content.message.replace(/\d+[,]\d+TL[\n]/gi, "");
+        content.message = content.message.replace("5,00TL", "");
+
+        content.message = content.message.split("\n");
+
+        const day = content.message[0];
+
+        content.message = content.message.slice(4);
+        content.message = content.message.slice(0, content.message.length - 1);
+
+        content.message = content.message.filter(function(n) {
+          return n != null && n.trim() != "";
+        });
+
+        content.message.forEach(function(part, index, theArray) {
+          theArray[index] = theArray[index].trim();
+        });
+
+        content.message = content.message.join("\n");
+
+        content.message = `[[ ${day} tarihli Sos Cafe Yemek Menüsü ]] \n` + "=============== \n" + content.message;
+
+        fetch(pushUrl, {
+          method: "POST",
+          headers: {
+            Accept: "application/json",
+            "Content-Type": "application/json"
+          },
+          body: JSON.stringify(content)
+        });
+      });
+  }
+  console.log(`File is ${req.query.file}`);
+  res.send(200, "Thank you.");
+  res.end();
+  next();
+}
+
+var server = restify.createServer();
+server.post("/hook", respond);
+
+server.use(restify.plugins.queryParser());
+server.use(restify.plugins.jsonBodyParser());
+server.use(restify.plugins.bodyParser());
+
+const trimContent = data => {};
+
+server.listen(8092, function() {
+  console.log("%s listening at %s", server.name, server.url);
+});
